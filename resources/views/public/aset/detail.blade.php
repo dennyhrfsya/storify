d4c95c264a771abd0db089f24d6cd141b0eaee6c
Denny Herfansya <dennyherfansya10@gmail.com>
2026-06-23 15:47:28 +0700

    Tambah teks Storify UI/UX

--- resources/views/public/aset/detail.blade.php
+++ resources/views/public/aset/detail.blade.php
@@ -14,6 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet">
 
+    {{-- Storify UI/UX --}}
     <link rel="stylesheet" href="{{ asset('css/storify-base-style-v2.css') }}">
 </head>
 
